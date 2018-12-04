02e5d23dc3ce18d76e2e78a70880879898764f1f
DeltaDizzy <EJBraswell@gmail.com>
2018-12-03 22:44:40 -0600

    Create project

---
+++ src/PlanetaryInfoScreen/PlanetaryInfoScreen/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PlanetaryInfoScreen")]
+[assembly: AssemblyDescription("A plugin for KSP that prints various celestial body characteristics to a log file")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Copyright (C) KSP-DDR 2018")]
+[assembly: AssemblyProduct("PlanetaryInfoScreen")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
+
+[assembly: KSPAssembly("PlanetaryInfoScreen", 0, 1)]
+[assembly: KSPAssemblyDependency("Kopernicus", 1, 0)]
+[assembly: KSPAssemblyDependency("Kopernicus.Components", 1, 0)]
+[assembly: KSPAssemblyDependency("Kopernicus.Parser", 1, 0)]
