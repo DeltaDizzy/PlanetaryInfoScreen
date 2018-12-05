6e3d50ecfa56f1dea3bb8e65256e2e81c8cef30c
DeltaDizzy <EJBraswell@gmail.com>
2018-12-04 23:36:15 -0600

    localizer and build

--- src/PlanetaryInfoScreen/PlanetaryInfoScreen/PrintFile.cs
+++ src/PlanetaryInfoScreen/PlanetaryInfoScreen/PrintFile.cs
@@ -1,15 +1,14 @@
-﻿using KSP.IO;
-using System;
-using Kopernicus;
-using System.Runtime.InteropServices;
-using UnityEngine;
+﻿using UnityEngine;
 using ModularFI;
 using Logger = Kopernicus.Logger;
+using KSP.Localization;
 
 namespace PlanetaryInfoScreen
 {
     public class PrintFile : MonoBehaviour
     {
+        public string fluxMsg = "Solar Flux for " + FlightGlobals.ActiveVessel.GetCurrentOrbit().referenceBody + "is " + ModularFlightIntegrator.ActiveVesselFI.solarFlux + " Watts/m^2";
+        public string eclipseMsg = "You are not in direct Sunlight!  Plase wait until day to measure again.";
         //print
         public void Write()
         {
@@ -23,11 +22,11 @@ namespace PlanetaryInfoScreen
         {
             if ((Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.V)) && FlightGlobals.ActiveVessel.directSunlight)
             {
-                ScreenMessages.PostScreenMessage("Solar Flux for " + FlightGlobals.ActiveVessel.GetCurrentOrbit().referenceBody + "is " + ModularFlightIntegrator.ActiveVesselFI.solarFlux + " Watts/m^2");
+                ScreenMessages.PostScreenMessage(Localizer.Format(fluxMsg));
             }
             else
             {
-                ScreenMessages.PostScreenMessage("You are not in direct Sunlight!  Plase wait until day to measure again.");
+                ScreenMessages.PostScreenMessage(Localizer.Format(eclipseMsg));
             }
         }
     }
